4774bab21748169d34aac76172b93d394962ea0f
FightFarewellFearless <faizakbar088@gmail.com>
2025-03-01 11:25:23 +0700

    Increase concurrency setting in remotion.config.ts for enhanced performance

--- remotion.config.ts
+++ remotion.config.ts
@@ -8,4 +8,4 @@ import { Config } from "@remotion/cli/config";
 Config.setVideoImageFormat("jpeg");
 Config.setOverwriteOutput(true);
 Config.setChromiumMultiProcessOnLinux(true);
-Config.setConcurrency(2);
+Config.setConcurrency(4);
