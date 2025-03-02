db412f83d29f9f970b0b652ebe45aab2ca109fb2
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2025-03-02 09:37:58 +0700

    Update remotion.config.ts

--- remotion.config.ts
+++ remotion.config.ts
@@ -7,5 +7,5 @@ import { Config } from "@remotion/cli/config";
 
 Config.setVideoImageFormat("jpeg");
 Config.setOverwriteOutput(true);
-//Config.setChromiumMultiProcessOnLinux(true);
-Config.setConcurrency(4);
+Config.setChromiumMultiProcessOnLinux(true);
+Config.setConcurrency(2);
