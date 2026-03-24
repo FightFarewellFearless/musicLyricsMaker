ad8f6790fb17777b38ef9c7c114bbcdb02a26452
pirles <83485510+FightFarewellFearless@users.noreply.github.com>
2026-03-24 16:34:36 +0700

    Update downloadAudio.js

--- downloadAudio.js
+++ downloadAudio.js
@@ -80,7 +80,7 @@ const pot = await BG.Challenge.create(bgConfig).then(async (bg) => {
 console.log("Creating Innertube instance with PoToken...");
 const innertube = await Innertube.create({
   // cache: new UniversalCache(true),
-  player_id: '00c52fa0',
+  //player_id: '00c52fa0',
   cookie: process.env.YT_COOKIE,
   po_token: pot,
   visitor_data: visitorData,
